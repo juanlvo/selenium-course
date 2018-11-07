ed55fb9f1acf96bf918cce7eeb55094e9a3c545e
juanlvo <juanlvo@gmail.com>
2018-11-07 15:35:26 +0100

    feat: lecture 66: runing test on google chrome

---
+++ src/basicweb/ChromeDriveDemo.java
@@ -0,0 +1,19 @@
+package basicweb;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeDriveDemo {
+
+	public static void main(String[] args) {
+		
+		String baseUrl = "http://www.google.com";
+		WebDriver driver;
+		
+		System.setProperty("webdriver.chrome.driver", "/home/juan/Documents/Selenium/chromedriver");
+		driver = new ChromeDriver();
+		
+		driver.get(baseUrl);
+	}
+
+}
