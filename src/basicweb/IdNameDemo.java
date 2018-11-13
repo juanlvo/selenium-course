7b8ef525b8fdfae397b86e3f10a2bcaaedb9b98b
Juan-Luis Vivas Occhipinti <Juan-Luis.Vivas@sogeti.lu>
2018-11-13 11:46:12 +0100

    feat: section 13 lecture 72 implementation of a search of an element by name

--- src/basicweb/IdNameDemo.java
+++ src/basicweb/IdNameDemo.java
@@ -1,10 +1,27 @@
 package basicweb;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/***
+ * Program for searching an element by name
+ * 
+ * @author Juan-Luis.Vivas
+ *
+ */
 public class IdNameDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		WebDriver driver;
+		System.setProperty("webdriver.gecko.driver", "C:\\selenium\\gecko32\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		String baseUrl = "https://www.google.com";
+		driver.manage().window().maximize();
+		driver.get(baseUrl);
+		
+		driver.findElement(By.id("lst-ib")).sendKeys("letskodeit");
+		driver.findElement(By.name("btnK")).click();
 	}
 
 }
