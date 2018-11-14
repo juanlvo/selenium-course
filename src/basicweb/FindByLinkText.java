b6e8327e21809d7fbc9ebc289d38da3847ed9209
Juan-Luis Vivas Occhipinti <Juan-Luis.Vivas@sogeti.lu>
2018-11-14 12:14:00 +0100

    feat: Section 13 Lecture 73 implementation of the search by link text. comment: When I execute the test in the machine of the office it doesn't work the loading of the page, I will need to retest at home

---
+++ src/basicweb/FindByLinkText.java
@@ -0,0 +1,31 @@
+package basicweb;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class FindByLinkText {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver;
+		System.setProperty("webdriver.gecko.driver", "C:\\selenium\\gecko32\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		//System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver.exe");
+		//driver = new ChromeDriver();
+		//System.setProperty("webdriver.ie.driver", "C:\\selenium\\IEDriverServer.exe");
+		//driver = new InternetExplorerDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); //really important for avoid timeouts
+		String baseURL = "https://learn.letskodeit.com/";
+		driver.get(baseURL);
+		
+		driver.findElement(By.linkText("Login")).click();
+		Thread.sleep(3000);
+		
+		driver.findElement(By.partialLinkText("Pract")).click();
+	}
+
+}
