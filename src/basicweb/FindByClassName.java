5a14c03c4a20d21378a4840be5137ed0d63ae576
Juan-Luis Vivas Occhipinti <Juan-Luis.Vivas@sogeti.lu>
2018-11-14 14:55:30 +0100

    feat: Section 13 Lecture 74 finding elements by class

--- src/basicweb/FindByClassName.java
+++ src/basicweb/FindByClassName.java
@@ -1,10 +1,22 @@
 package basicweb;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class FindByClassName {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		WebDriver driver;
+		System.setProperty("webdriver.gecko.driver", "C:\\selenium\\gecko32\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		String baseURL = "https://demostore.x-cart.com/";
+		driver.manage().window().maximize();
+		driver.get(baseURL);
 
+		driver.findElement(By.className("title")).click();
+		
+		driver.findElement(By.className("leaf")).click(); //is going to fail because is not finding the element
 	}
 
 }
