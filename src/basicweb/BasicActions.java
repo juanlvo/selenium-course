55c308e41c9ffd16a782a5006f0caf0c88f8eeda
Juan-Luis Vivas Occhipinti <juanlvo@gmail.com>
2018-11-27 14:54:27 +0100

    feat: lecture 90 section 16 JUnit Selenium sending keys, clearing fields and making click in buttons and links

---
+++ src/basicweb/BasicActions.java
@@ -0,0 +1,45 @@
+package basicweb;
+
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+class BasicActions {
+
+	WebDriver driver;
+	String baseUrl;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		System.setProperty("webdriver.gecko.driver", "C:\\selenium\\gecko32\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		baseUrl = "https://letskodeit.teachable.com/";
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+	}
+
+	@Test
+	void test() {
+		driver.get(baseUrl);
+		driver.findElement(By.xpath("//a[@class='navbar-link fedora-navbar-link']")).click();
+		System.out.println("Click on login");
+		driver.findElement(By.id("user_email")).sendKeys("test@email.com");
+		System.out.println("Sending keys to user name field");
+		driver.findElement(By.id("user_password")).sendKeys("test");
+		System.out.println("Sending keys to password field");
+		driver.findElement(By.id("user_email")).clear();
+		System.out.println("Clearing the user email");
+	}
+	
+	@AfterEach
+	void tearDown() throws Exception {
+		//driver.quit();
+	}
+
+}
