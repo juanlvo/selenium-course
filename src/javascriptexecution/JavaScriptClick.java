96187719ed6856dea3907899836c7107f99d9a7c
Juan Vivas <juanlvo@gmail.com>
2018-12-28 10:42:49 +0100

    feat: Section 20 Lecture 122 forzing click when selenium is not able to click (JavaScript) Example

--- src/javascriptexecution/JavaScriptClick.java
+++ src/javascriptexecution/JavaScriptClick.java
@@ -34,14 +34,34 @@ class JavaScriptClick {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 
-	@Test
-	public void test() throws InterruptedException {
+/*	@Test
+	public void testJSClick1() throws InterruptedException {
 		//Navigation
 		js.executeScript("window.location = 'https://learn.letskodeit.com/p/practice';");
 		Thread.sleep(2000);
 		
 		WebElement checkbox = driver.findElement(By.id("bmwcheck"));
 		js.executeScript("arguments[0].click();", checkbox);
+	}*/
+	
+	@Test
+	public void testJSClick2() throws InterruptedException {
+		//Navigation
+		js.executeScript("window.location = 'https://www.retailmenot.com/';");
+		Thread.sleep(2000);
+		
+		driver.findElement(By.xpath("//*[@class='icon icon-close']")).click();
+		WebElement linkLogin = driver.findElement(By.xpath("/html/body/div[1]/div[1]/header/div/div/div/div[2]/a"));
+		js.executeScript("arguments[0].click();", linkLogin);
+		
+		Thread.sleep(3000);
+		
+		WebElement checkBox = driver.findElement(By.id("subscribe"));
+		
+		System.out.println("Displayed: "+checkBox.isDisplayed());
+		System.out.println("Selected: "+checkBox.isSelected());
+		
+		js.executeScript("arguments[0].click();", checkBox);
 	}
 	
 	@AfterEach
